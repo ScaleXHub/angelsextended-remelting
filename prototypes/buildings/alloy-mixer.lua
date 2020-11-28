6e96a9b7762c4ec0199a9cf8eda054c2a5022c98
@Medved2k <alexey.victorovich.kiselev@yandex.ru>
2020-11-28 14:51:03 +0700

    Fix launch issues with missed icons from angelperochem

--- prototypes/buildings/alloy-mixer.lua
+++ prototypes/buildings/alloy-mixer.lua
@@ -9,7 +9,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_1.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_1.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -29,7 +29,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_1.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_1.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -121,7 +121,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_2.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_2.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -141,7 +141,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_2.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_2.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -232,7 +232,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_3.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_3.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -252,7 +252,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_3.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_3.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -343,7 +343,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_4.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_4.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
@@ -363,7 +363,7 @@ data:extend(
 				icon = "__angelsextended-remelting__/graphics/icons/alloy-mixer.png",
 			},
 			{
-				icon = "__angelspetrochem__/graphics/icons/num_4.png",
+				icon = "__angelsextended-remelting__/graphics/icons/num_4.png",
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12},
